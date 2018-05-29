fad517265bd047424b4e8222e90e30300b91bc37
Akulich Evgeniy <zhenya-akulich@mail.ru>
2018-05-29 09:50:37 +0300

    First commit; (+) "Home" controller, "Index" method in it

---
+++ HelperMethods/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelperMethods.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            ViewBag.Fruits = new string[] { "Яблоко", "Апельсин", "Груша" };
+            ViewBag.Cities = new string[] { "Москва", "Лондон", "Париж" };
+
+            string message = "Это HTML-элемент: <input>";
+
+            return View((object)message);
+        } // end Index()
+
+
+
+
+
+
+
+
+
+    } // end class
+} // end namespace
