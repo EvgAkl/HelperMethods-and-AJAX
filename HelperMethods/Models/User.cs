946840868e7898d9b3f2eb9ae43354e11d326536
Akulich Evgeniy <zhenya-akulich@mail.ru>
2018-05-29 12:43:29 +0300

    (+) "User" class in "Models" folder

---
+++ HelperMethods/Models/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HelperMethods.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string firstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Addres HomeAddress { get; set; }
+        public bool IsApproved { get; set; }
+        public Role Role { get; set; }
+
+    }
+
+    public class Addres
+    {
+        public string Line1 { get; set; }
+        public string line2 { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+    }
+
+    public enum Role
+    {
+        Admin,
+        User,
+        Guest
+    }
+
+} // end namsespace
