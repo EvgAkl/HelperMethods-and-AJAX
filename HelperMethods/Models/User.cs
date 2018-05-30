445cf22a3c7379eda571fa257db0e512ed515900
Akulich Evgeniy <zhenya-akulich@mail.ru>
2018-05-30 10:53:16 +0300

    (+) Metadata for class "User"

--- HelperMethods/Models/User.cs
+++ HelperMethods/Models/User.cs
@@ -1,17 +1,29 @@
 ﻿using System;
+using System.Web.Mvc;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace HelperMethods.Models
 {
+    [DisplayName("новый пользователь")]
     public class User
     {
+        [HiddenInput(DisplayValue = false)]
         public int UserId { get; set; }
+        [DisplayName("Имя")]
+        [UIHint("MultilineText")]
         public string FirstName { get; set; }
+        [DisplayName("Фамилия")]
         public string LastName { get; set; }
+        [DisplayName("Дата рождения")]
+        [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
+        [DisplayName("Адрес")]
         public Addres HomeAddress { get; set; }
+        [DisplayName("Подтвердил регистрацию")]
         public bool IsApproved { get; set; }
+        [DisplayName("Роль")]
         public Role Role { get; set; }
-
     }
 
     public class Addres
