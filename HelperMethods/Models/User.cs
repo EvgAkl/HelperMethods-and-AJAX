df1e559cb03c77f1a99b2969988cd95f08db947f
Akulich Evgeniy <zhenya-akulich@mail.ru>
2018-05-30 12:05:15 +0300

    Define metadata "User" partial class in "UserMetadata" partial class

--- HelperMethods/Models/User.cs
+++ HelperMethods/Models/User.cs
@@ -1,28 +1,18 @@
 ﻿using System;
-using System.Web.Mvc;
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace HelperMethods.Models
 {
-    [DisplayName("новый юзер")]
-    public class User
+    [MetadataType(typeof(UserMetadata))]
+    public partial class User
     {
-        [HiddenInput(DisplayValue=false)]
         public int UserId { get; set; }
-        [DisplayName("Имя")]
-        [UIHint("MultilineText")]
         public string FirstName { get; set; }
-        [DisplayName("Фамилия")]
         public string LastName { get; set; }
-        [DisplayName("Дата рождения")]
         [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
-        [DisplayName("Адрес")]
         public Addres HomeAddress { get; set; }
-        [DisplayName("Подтвердил регистрацию?")]
         public bool IsApproved { get; set; }
-        [DisplayName("Роль")]
         public Role Role { get; set; }
     }
 
---
+++ HelperMethods/Models/UserMetadata.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web.Mvc;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace HelperMethods.Models
+{
+    [DisplayName("новый юзер")]
+    public partial class UserMetadata
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int UserId { get; set; }
+        [DisplayName("Имя")]
+        [UIHint("MultilineText")]
+        public string FirstName { get; set; }
+        [DisplayName("Фамилия")]
+        public string LastName { get; set; }
+        [DisplayName("Дата рождения")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+        [DisplayName("Адрес")]
+        public Addres HomeAddress { get; set; }
+        [DisplayName("Подтвердил регистрацию?")]
+        public bool IsApproved { get; set; }
+        [DisplayName("Роль")]
+        public Role Role { get; set; }
+    } // end class
+
+} // end namespace
